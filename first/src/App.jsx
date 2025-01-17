0ac0a1dadd8aca64c3508841b211a819c0005239
ArjunGoud7 <akulaarjun18@gmail.com>
2025-01-17 14:59:48 +0530

    Random Users Data Fetching 1

---
+++ first/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Nav from "./components/nav/Nav"
+import User from "./components/user/User"
+function App() {
+  return (
+    <div>
+      <Nav />
+      <br/>
+      <User />
+    </div>
+  )
+}
+
+export default App
